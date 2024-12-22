9ebb0906fd3e2077d5573c38563b36c6eb71c45c
dna281086 <dna281086@gmail.com>
2024-12-22 19:08:43 +0300

    Add files via upload

---
+++ module_16_1.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def read_root() -> str:
+    return (f'Главная страница')
+
+
+@app.get("/user/admin")
+async def read_root() -> str:
+    return (f'Вы вошли как администратор')
+
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: int):
+    return (f'Вы вошли как пользователь № {user_id}')
+
+
+@app.get('/user')
+async def get_user_info(username: str, age: int):
+    return {f'Информация о пользователе. Имя: {username}, Возраст: {age}'}
